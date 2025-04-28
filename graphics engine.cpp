52f335721fb759e3c7558a1658af7bae0cda60c9
prof_redstone <tom.demagny@laposte.net>
2025-04-29 00:20:20 +0200

    first push

---
+++ graphics engine.cpp
@@ -0,0 +1,16 @@
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
